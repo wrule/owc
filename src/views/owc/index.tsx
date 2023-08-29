054b06dd50b941e9b0a988b64158e1c32fc54f1a
jimao <jimao@jimaodeMacBook-Pro.local>
2023-08-29 18:03:29 +0800

    思考页面设计

--- src/views/owc/index.tsx
+++ src/views/owc/index.tsx
@@ -1,8 +1,16 @@
+import { Col, Row, Spin } from 'antd';
 import React from 'react';
+import progress from './progress.gif';
 
 export
 function OWC() {
-  return <div>
-    OWC
-  </div>;
+  return <Spin size="large" spinning={true} tip={<span style={{ fontSize: '1rem' }}>数据加载中...</span>}>
+    <Row>
+      <Col span={12}>
+        {/* <img src={progress} /> */}
+      </Col>
+      <Col span={12}>
+      </Col>
+    </Row>
+  </Spin>;
 }
