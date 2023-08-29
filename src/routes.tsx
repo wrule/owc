3808da714f0919b57cc4a1b965f9ac58452f6211
jimao <jimao@jimaodeMacBook-Pro.local>
2023-08-29 17:45:38 +0800

    owc页面

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './views/home';
 import { Test } from './views/test';
+import { OWC } from './views/owc';
 
 export
 function RouteView() {
   return <Routes>
     <Route path="" element={<Home />} />
+    <Route path="owc" element={<OWC />} />
     <Route path="test" element={<Test />} />
   </Routes>;
 }
---
+++ src/views/owc/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export
+function OWC() {
+  return <div>
+    OWC
+  </div>;
+}
