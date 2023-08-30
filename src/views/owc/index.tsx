4a6aea14b34b7f91eb84a75466b3b7ce8eecb73b
jimao <jimao@jimaodeMacBook-Pro.local>
2023-08-30 17:21:05 +0800

    fix: bug

--- src/views/owc/index.tsx
+++ src/views/owc/index.tsx
@@ -1,13 +1,45 @@
-import { Col, Progress, Row, Spin } from 'antd';
-import React, { useState } from 'react';
+import { Button, Col, Progress, Row, Spin } from 'antd';
+import React, { useEffect, useState } from 'react';
 import progress from './progress.gif';
 import style from './index.module.scss';
+import axios from 'axios';
+
+
+async function fetchResource(url: string) {
+  const cacheUrl = localStorage.getItem(url);
+  if (cacheUrl != null) {
+    try {
+      const res = await axios.get(cacheUrl);
+      return res.data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  const res = await axios.get(url);
+  const blob = new Blob([res.data], { type: res.headers['content-type'] });
+  const objectUrl = URL.createObjectURL(blob);
+  localStorage.setItem(url, objectUrl);
+  return res.data;
+}
 
 function Ready() {
-  const [scriptProgress, setScriptProgress] = useState<number>(99);
-  const [dataProgress, setDataProgress] = useState<number>(100);
+  const [scriptProgress, setScriptProgress] = useState<number>(0);
+  const [dataProgress, setDataProgress] = useState<number>(0);
+
+  const load = async () => {
+    console.log(1);
+    const rsp = await fetchResource('/data.json');
+    console.log(2);
+  };
+
+  useEffect(() => {
+    load();
+  }, []);
 
   return <div className={style.com}>
+    <Button onClick={() => {
+      window.open(window.location.href);
+    }}>Fork</Button>
     <Row>
       <span>{`Script loading${scriptProgress === 100 ? ' complete' : '...'}`}</span>
       <Progress percent={scriptProgress} />
