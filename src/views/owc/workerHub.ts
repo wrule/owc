ced2f70439c740b9aad3d95409ed2d0869e52bbe
jimao <jimao@jimaodeMacBook-Pro.local>
2023-08-31 14:45:12 +0800

    ChangeWorkers

--- src/views/owc/workerHub.ts
+++ src/views/owc/workerHub.ts
@@ -1,17 +1,37 @@
 
 export
+interface Best {
+  value: number;
+  params: any;
+}
+
+export
 class WorkerHub {
   public constructor(private scriptURL: string) {
 
   }
 
   private workers: Worker[] = [];
+  private iterations = 0;
+  private best: Best = { value: -Infinity, params: { } };
+
+  private createWorker() {
+    const worker = new Worker(this.scriptURL);
+    worker.onmessage = (event) => {
+      const type: string = event.data?.type;
+      const data: any = event.data?.data;
+      if (type === 'iterations') this.iterations += data ?? 0;
+      if (type === 'best' && data?.value > this.best.value) {
+        console.log(data);
+      }
+    };
+    return worker;
+  }
 
   public ChangeWorkers(num: number) {
     let diff = num - this.workers.length;
-    if (diff > 0) {
-      this.workers.push(...Array(diff).fill(0).map(() => new Worker(this.scriptURL)));
-    }
+    if (diff > 0)
+      this.workers.push(...Array(diff).fill(0).map(() => this.createWorker()));
     if (diff < 0) {
       diff = -diff;
       while (diff > 0 && this.workers.length > 1)
