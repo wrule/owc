d894fef840f9cd8855992a0208c8e8ddc4da20ee
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-01 14:16:39 +0800

    删除dist

--- server/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function hello(): void;
-//# sourceMappingURL=index.d.ts.map
--- server/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
-function hello() {
-    console.log('hello world!');
-}
-exports.hello = hello;
-//# sourceMappingURL=index.js.map
