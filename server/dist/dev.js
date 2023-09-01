fa69965902a0641d3e16de76ecf9cc5a5285f175
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-01 12:02:10 +0800

    添加后端

---
+++ server/dist/dev.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const _1 = require(".");
+function dev() {
+    (0, _1.hello)();
+}
+dev();
+//# sourceMappingURL=dev.js.map
---
+++ server/dist/index.d.ts
@@ -0,0 +1,2 @@
+export declare function hello(): void;
+//# sourceMappingURL=index.d.ts.map
