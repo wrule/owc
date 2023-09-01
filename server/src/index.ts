ea6114fd4663b7a72fdbd49b4d9ba721b4538233
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-01 14:39:45 +0800

    fillParams引入

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,22 @@ import http from 'http';
 import path from 'path';
 import Express from 'express';
 import SocketIO from 'socket.io';
+import * as args from '@wrule/args';
 
 const webPath = 'build';
 
 interface Best {
   value: number;
   params: any;
+}
+
+function fillParams(params: any) {
+  Object.entries(args.parse(process.argv)).forEach(([key, value]) => {
+    if (!(key in params)) throw `unknown param ${key}`;
+    params[key] = value;
+  });
+  console.log(`./${args.file_name(process.argv[1])} ${args.stringify(params)}`);
+  console.log(params);
 }
 
 function socketServer(server: http.Server) {
