3382a2424304c60eb7ce6a89321dac5a43f85555
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-01 12:38:41 +0800

    提交代码

--- server/dist/dev.js
+++ server/dist/dev.js
@@ -1,8 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require(".");
-function dev() {
-    (0, _1.hello)();
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const webPath = 'www';
+function main() {
+    const app = (0, express_1.default)();
+    app.use(express_1.default.static(path_1.default.resolve(webPath)));
+    app.get('*', (req, res) => res.sendFile(path_1.default.resolve(webPath, 'index.html')));
+    app.listen(9993);
+    console.log(9993);
 }
-dev();
+main();
 //# sourceMappingURL=dev.js.map
--- server/src/dev.ts
+++ server/src/dev.ts
@@ -1,7 +1,14 @@
-import { hello } from '.';
+import express from 'express';
+import path from 'path';
 
-function dev() {
-  hello();
+const webPath = 'www';
+
+function main() {
+  const app = express();
+  app.use(express.static(path.resolve(webPath)));
+  app.get('*', (req, res) => res.sendFile(path.resolve(webPath, 'index.html')));
+  app.listen(9993);
+  console.log(9993);
 }
 
-dev();
+main();
