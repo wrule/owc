8b4016cb49d807e833d5205a25b8d91f814fb1f0
jimao <jimao@jimaodeMacBook-Pro.local>
2023-09-01 13:27:58 +0800

    提交代码

--- server/dist/dev.js
+++ server/dist/dev.js
@@ -3,15 +3,41 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
 const express_1 = __importDefault(require("express"));
+const socket_io_1 = __importDefault(require("socket.io"));
 const path_1 = __importDefault(require("path"));
 const webPath = 'www';
+function socketServer(server) {
+    let best = { value: -Infinity, params: undefined };
+    let iterations = 0;
+    let workers = 0;
+    const io = new socket_io_1.default.Server(server);
+    io.on('connection', (client) => {
+        client.on('best', (newBest) => {
+            if (newBest.value > best.value) {
+                best = newBest;
+                io.emit('best', best);
+            }
+        });
+        client.on('iterations', (change) => {
+            iterations += change;
+            io.emit('iterations', iterations);
+        });
+        client.on('workers', (change) => {
+            workers += change;
+            io.emit('workers', workers);
+        });
+    });
+    return io;
+}
 function main() {
     const app = (0, express_1.default)();
+    const server = http_1.default.createServer(app);
+    socketServer(server);
     app.use(express_1.default.static(path_1.default.resolve(webPath)));
     app.get('*', (req, res) => res.sendFile(path_1.default.resolve(webPath, 'index.html')));
-    app.listen(9993);
-    console.log(9993);
+    server.listen(9999);
 }
 main();
 //# sourceMappingURL=dev.js.map
--- server/src/dev.ts
+++ server/src/dev.ts
@@ -1,14 +1,46 @@
+import http from 'http';
 import express from 'express';
+import SocketIO from 'socket.io';
 import path from 'path';
 
 const webPath = 'www';
 
+interface Best {
+  value: number;
+  params: any;
+}
+
+function socketServer(server: http.Server) {
+  let best: Best = { value: -Infinity, params: undefined };
+  let iterations = 0;
+  let workers = 0;
+  const io = new SocketIO.Server(server);
+  io.on('connection', (client) => {
+    client.on('best', (newBest: Best) => {
+      if (newBest.value > best.value) {
+        best = newBest;
+        io.emit('best', best);
+      }
+    });
+    client.on('iterations', (change: number) => {
+      iterations += change;
+      io.emit('iterations', iterations);
+    });
+    client.on('workers', (change: number) => {
+      workers += change;
+      io.emit('workers', workers);
+    });
+  });
+  return io;
+}
+
 function main() {
   const app = express();
+  const server = http.createServer(app);
+  socketServer(server);
   app.use(express.static(path.resolve(webPath)));
   app.get('*', (req, res) => res.sendFile(path.resolve(webPath, 'index.html')));
-  app.listen(9993);
-  console.log(9993);
+  server.listen(9999);
 }
 
 main();
